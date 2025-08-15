4736a8949258dab4bea5a01fd85c6b1374423952
server-machine <xxxx8350@gmail.com>
2025-08-15 14:51:35 +0300

    feat: update прошивки ардуино для send_message

--- core/servers/l2mad/zones/rows.py
+++ core/servers/l2mad/zones/rows.py
@@ -10,13 +10,19 @@ ZONES: Dict[str, ZoneDecl] = {
 }
 
 TEMPLATES: Dict[str, List[str]] = {
-    # примеры; клади реальные PNG в templates/ со своей структурой
-    "compass_n": ["world", "compass", "north.png"],
-    "landmark_bridge": ["world", "landmarks", "bridge.png"],
-    # ...
+
+    # клади реальные PNG в templates/ со своей структурой
+
+    # Interface
+    "autofarm": ["interface", "autofarm.png"],
 
     #Varka_1
     "Varka_1_capt1": ["rows", "Goddard", "VarkaSilenosStronghold", "Varka_1", "1.png"],
     "Varka_1_capt2": ["rows", "Goddard", "VarkaSilenosStronghold", "Varka_1", "2.png"],
     "Varka_1_capt3": ["rows", "Goddard", "VarkaSilenosStronghold", "Varka_1", "3.png"],
+
+    #Primeval_1
+    "Primeval_1_capt1": ["rows", "Rune", "PrimevalIsle", "Primeval_1", "1.png"],
+    "Primeval_1_capt2": ["rows", "Rune", "PrimevalIsle", "Primeval_1", "2.png"],
+    "Primeval_1_capt3": ["rows", "Rune", "PrimevalIsle", "Primeval_1", "3.png"],
 }
