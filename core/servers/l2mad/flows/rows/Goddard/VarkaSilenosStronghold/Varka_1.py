dee6c080a883e617b9cb426042f6c7fcb2d16062
server-machine <xxxx8350@gmail.com>
2025-08-15 16:47:59 +0300

    refactor: try to fix primeval zome template matching

--- core/servers/l2mad/flows/rows/Goddard/VarkaSilenosStronghold/Varka_1.py
+++ core/servers/l2mad/flows/rows/Goddard/VarkaSilenosStronghold/Varka_1.py
@@ -19,6 +19,6 @@ FLOW = [
     # {"op": "send_arduino", "cmd": "pageup", "delay_ms": 300},
     # {"op": "sleep", "ms": 2000},
     # {"op": "wait", "zone": "center_block", "tpl": "landmark_bridge", "timeout_ms": 6000, "thr": 0.88},
-    #     {"op": "send_arduino", "cmd": "pagedown", "delay_ms": 300},  # повернуть
+    #     {"op": "send_arduino", "cmd": "pagedown", "delay_ms": 300},  #
     #     {"op": "sleep", "ms": 2000},
-].ефкпуе
+]
